3f345b5eff891395146485d93b361c44b2c06015
Pedro Gama <pedro.gama@ufms.br>
2025-10-24 16:37:43 -0400

    Add new components and integrate Tailwind CSS
    Added AnaliseGrafica, Historico, and PerfilStatus components with corresponding CSS files. Integrated Tailwind CSS and @tailwindcss/vite into the project, updated App.tsx, removed unused SVG assets, and adjusted Vite config and index.css for new styling.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import PerfilStatus from './components/PerfilStatus'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='bg-gray-900 text-white min-h-screen'>
+      <PerfilStatus />  
+    </div>
   )
 }
 
